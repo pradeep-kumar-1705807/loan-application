e28df2533ead5a67f02cab082f67d1f96eb3feac
pradeep-kumar-1705807 <kumarpradeep2008.2@gmail.com>
2021-02-04 15:59:37 +0530

    pradeep- not null validation done

--- WebContent/WEB-INF/view/newform.jsp
+++ WebContent/WEB-INF/view/newform.jsp
@@ -19,14 +19,20 @@
 	<form:input placeholder="First Name" path="firstName" /> 
 	<form:errors path="firstName" cssClass="error" /> <br>
 
-	<!--  <label for="mn">Middle Name </label>
-	<form:input placeholder="Middle Name" path="midName" /><br>
+	  <label for="mn">Middle Name </label>
+	<form:input placeholder="Middle Name" path="midName" />
 	
-	<label for="ln">Last Name </label>
-	<form:input placeholder="Last Name" path="lastName" /><br>
+	<br>
+	
+	 <label for="ln">Last Name </label>
+	<form:input placeholder="Last Name" path="lastName" />
+	<form:errors path="lastName" cssClass="error" /> <br>
+	
+	<br>
 	
 	<label for="birthday">Date of Birth </label>
-	<form:input type = "date" path="dob"/><br>
+	<form:input type = "date" path="dob"/>
+	<form:errors path="dob" cssClass="error" /> <br>
 	
 	<label for="marriage">Marital Status </label>
 	  <form:select path="maritalStatus">
@@ -35,88 +41,141 @@
 	    <form:option value="Separated" label="Separated"/>
 	    <form:option value="Divorced" label="Divorced"/>
 	    <form:option value="Widowed" label="Widowed"/>
-	  </form:select><br>
+	  </form:select>
+	  
+	  <form:errors path="maritalStatus" cssClass="error" />
+	  
+	  <br>
 	  
 	 <label for="ssn">SSN Number </label>
-	 <form:input placeholder="SSN Number" path="ssnNumber"/><br>
+	 <form:input placeholder="SSN Number" path="ssnNumber"/>
+	 <form:errors path="ssnNumber" cssClass="error" />
+	 <br>
 	 
 	 <label for="lamt">Loan Amount</label>
-	 <form:input placeholder="Loan Amount" path="loanAmt"/><br>
+	 <form:input placeholder="Loan Amount" path="loanAmt"/>
+	 <form:errors path="loanAmt" cssClass="error" />
+	 <br>
 	 
-	 <label for="lpurpose">Loan Purpose</label>
+	   <label for="lpurpose">Loan Purpose</label>
 	  <form:select path="loanPurpose">
 	  	<form:option value="Debt" label="Debt"/>
 	    <form:option value="Education Loan" label="Education Loan"/>
 	    <form:option value="Personal Loan" label="Personal Loan"/>
 	    <form:option value="Home Loan" label="Home Loan"/>
-	  </form:select><br>
+	  </form:select>
 	  
-	 <label for="description">Description</label>
+	  <form:errors path="loanPurpose" cssClass="error" />
+	  <br>
+	  
+	  <label for="description">Description</label>
 	 <form:textarea path="desc" rows="4" cols="50"></form:textarea><br>
 	 
 	 <label for="address">Address</label><br>
 	 
 	 <label for="addressline1">Address Line 1</label>
-	 <form:input placeholder="Address Line 1" path="addrOne" /> <br>
+	 <form:input placeholder="Address Line 1" path="addrOne" /> 
+	 <form:errors path="addrOne" cssClass="error" />
+	 
+	 <br>
 	 
 	 <label for="addressline2">Address Line 2</label>
 	 <form:input placeholder="Address Line 2" path="addrTwo" /> <br>
 	 
 	 <label for="cty">City</label>
-	 <form:input placeholder="City" path="city" /> <br>
+	 <form:input placeholder="City" path="city" /> 
+	 <form:errors path="city" cssClass="error" />
+	 <br>
 	 
 	 <label for="stt">State</label>
-	 <form:input placeholder="State" path="state" /> <br>
+	 <form:input placeholder="State" path="state" /> 
+	 <form:errors path="state" cssClass="error" />
+	 <br>
 	 
 	 <label for="pcode">Postal Code</label>
-	 <form:input placeholder="Postal Code" path="pincode" /> <br>
+	 <form:input placeholder="Postal Code" path="pincode" /> 
+	 <form:errors path="pincode" cssClass="error" />
+	 <br>
 	 
 	 <label for="contact">Contact Information</label><br>
 	 
 	 <label for="home">Home Phone</label>
-	 <form:input placeholder="123456" path="homePhone" /> <br>
+	 <form:input placeholder="123456" path="homePhone" /> 
+	 <form:errors path="homePhone" cssClass="error" />
+	 <br>
+	 
 	 
 	 <label for="office">Office Phone</label>
-	 <form:input placeholder="123456" path="officePhone" /> <br>
+	 <form:input placeholder="123456" path="officePhone" />
+	  <br>
 	 
 	 <label for="mobi">Mobile</label>
-	 <form:input placeholder="123456" path="mobile" /> <br>
+	 <form:input placeholder="123456" path="mobile" /> 
+	 <form:errors path="mobile" cssClass="error" />
+	 <br>
 	 
 	 <label for="mail">Email Address</label>
-	 <form:input placeholder="test@test.com" path="email" /> <br>
-	 
-	 <label for="empdetails">Employment Details</label><br>
+	 <form:input placeholder="test@test.com" path="email" />
+     <form:errors path="email" cssClass="error" />
+	 <br>
+	 --
+	  <label for="empdetails">Employment Details</label><br>
 	 
 	 <label for="ename">Employer Name</label>
-	 <form:input placeholder="name" path="empName" /> <br>
+	 <form:input placeholder="name" path="employeeName" /> 
+	  <form:errors path="employeeName" cssClass="error" />
+	 <br>
 	 
 	 <label for="salary">Annual Salary</label>
-	 <form:input placeholder="123456" path="annualSal" /> <br>
+	 <form:input placeholder="123456" path="annualSal" /> 
+	 <form:errors path="annualSal" cssClass="error" />
+	 
+	 <br>
 	 
 	 <label for="empaddress">Address</label><br>
 	 
 	 <label for="eaddressline1">Address Line 1</label>
-	 <form:input placeholder="Address Line 1" path="eAddrOne" /> <br>
+	 <form:input placeholder="Address Line 1" path="employeeAddressOne" />
+	 <form:errors path="employeeAddressOne" cssClass="error" />
+	  
+	 
+	 <br>
 	 
 	 <label for="eaddressline2">Address Line 2</label>
-	 <form:input placeholder="Address Line 2" path="eAddrTwo" /> <br>
+	 <form:input placeholder="Address Line 2" path="employeeAddressTwo" /> 
+	 
+	 <br>
 	 
 	 <label for="ecity">City</label>
-	 <form:input placeholder="City" path="eCity" /> <br>
+	 <form:input placeholder="City" path="employeeCity" /> 
+	 <form:errors path="employeeCity" cssClass="error" />
+	 <br>
 	 
 	 <label for="estate">State</label>
-	 <form:input placeholder="State" path="eState" /> <br>
+	 <form:input placeholder="State" path="employeeState" /> 
+	 <form:errors path="employeeState" cssClass="error" />
+	 <br>
 	 
 	 <label for="epincode">Postal Code</label>
-	 <form:input placeholder="Postal Code" path="ePincode" /> <br>
+	 <form:input placeholder="Postal Code" path="employeePincode" /> 
+	  <form:errors path="employeePincode" cssClass="error" />
+	 
+	 <br>
 	 
 	 <label for="exp">Experience</label>
-	 <form:input placeholder="Years" path="years" /> <form:input placeholder="Months" path="months" /> <br><br> -->
+	 <form:input placeholder="Years" path="years" /> 
+	   <form:errors path="years" cssClass="error" />
+	 
+	 <form:input placeholder="Months" path="months" /> 
+	 <form:errors path="months" cssClass="error" />
+	 
+	 <br><br> 
 	<input type="submit" value="submit"/>
 
 
 
 </form:form>
+
 
 </body>
 </html>
--- src/com/dynamicdudes/model/LoanApplicant.java
+++ src/com/dynamicdudes/model/LoanApplicant.java
@@ -1,220 +1,405 @@
 package com.dynamicdudes.model;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
 public class LoanApplicant {
 	
 	@NotNull(message="is Required")
-	@Size(min=1,message="is Required")
+	@Size(min=1,message="min 1 character Required")
+	@Size(max=255,message="255 character limit")
 	private String firstName;
 	
 	
+	
+	private String midName;
+	
 	@NotNull(message="is Required")
-	@Size(min=1,message="is Required")
+	@Size(min=1,message="min 1 character Required")
+	@Size(max=255,message="255 character limit")
 	private String lastName;
 	
-	@NotNull(message="is Required")
-	@Size(min=1,message="is Required")
-	private String midName; 
 	
+	 
+	
+	@NotNull(message="is Required")
 	
 	private String dob;
-	private String maritalStatus;
-	private String loanPurpose;
-	private String desc;
-	private String addrOne;
-	private String addrTwo;
-	private String city;
-	private String state;
-	private String email;
-	private String empName;
-	private String designation;
-	private String eAddrOne; 
-	private String eAddrTwo;
-	private String eCity;
-	private String eState;
 	
-	private Integer ssnNumber;
-	private Integer homePhone; 
-	private Integer officePhone;
-	private Integer  mobile;
-	private Integer  pincode;
+	@NotNull(message="is Required")
+	private String maritalStatus;
+	
+	@NotNull(message="is Required")
+	
+	private String loanPurpose;
+	
+	
+	private String desc;
+	
+	@NotNull(message="is Required")
+	@Size(min=1,message="min 1 character Required")
+	@Size(max=255,message="255 character limit")
+	private String addrOne;
+	
+	
+	private String addrTwo;
+	
+	@NotNull(message="is Required")
+	@Size(min=1,message="min 1 character Required")
+	@Size(max=255,message="255 character limit")
+	private String city;
+	
+	@NotNull(message="is Required")
+	@Size(min=1,message="min 1 character Required")
+	@Size(max=255,message="255 character limit")
+	private String state;
+	
+	
+	@NotNull(message="is Required")
+	private String email;
+	
+	
+	@NotNull(message="is Required")
+	private String employeeName;
+	
+	
+	
+	private String designation;
+	
+	
+	@NotNull(message="is Required")
+	private String employeeAddressOne; 
+	
+	private String employeeAddressTwo;
+	
+	
+	@NotNull(message="is Required")
+	private String employeeCity;
+	
+	
+	@NotNull(message="is Required")
+	private String employeeState;
+	
+	
+	@NotNull(message="is Required")
+	private Long ssnNumber;
+	
+	
+	@NotNull(message="is Required")
+	private Long homePhone; 
+	
+	
+	
+	private Long officePhone;
+	
+	@NotNull(message="is Required")
+	private Long mobile;
+	
+	
+	@NotNull(message="is Required")
+	private Long  pincode;
+	
+	
+	@NotNull(message="is Required")
 	private Integer years;
+	
+	
+	@NotNull(message="is Required")
 	private Integer  months;
-	private Integer  ePincode;
+	
+	
+	@NotNull(message="is Required")
+	private Integer  employeePincode;
+	
+	
+	@NotNull(message="is Required")
 	private Double loanAmt;
+	
+	
+	@NotNull(message="is Required")
 	private Double annualSal;
+
+
 	public String getFirstName() {
 		return firstName;
 	}
+
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+
+
 	public String getMidName() {
 		return midName;
 	}
+
+
 	public void setMidName(String midName) {
 		this.midName = midName;
 	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
 	public String getDob() {
 		return dob;
 	}
+
+
 	public void setDob(String dob) {
 		this.dob = dob;
 	}
+
+
 	public String getMaritalStatus() {
 		return maritalStatus;
 	}
+
+
 	public void setMaritalStatus(String maritalStatus) {
 		this.maritalStatus = maritalStatus;
 	}
+
+
 	public String getLoanPurpose() {
 		return loanPurpose;
 	}
+
+
 	public void setLoanPurpose(String loanPurpose) {
 		this.loanPurpose = loanPurpose;
 	}
+
+
 	public String getDesc() {
 		return desc;
 	}
+
+
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
+
+
 	public String getAddrOne() {
 		return addrOne;
 	}
+
+
 	public void setAddrOne(String addrOne) {
 		this.addrOne = addrOne;
 	}
+
+
 	public String getAddrTwo() {
 		return addrTwo;
 	}
+
+
 	public void setAddrTwo(String addrTwo) {
 		this.addrTwo = addrTwo;
 	}
+
+
 	public String getCity() {
 		return city;
 	}
+
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+
 	public String getState() {
 		return state;
 	}
+
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getEmpName() {
-		return empName;
+
+
+	public String getEmployeeName() {
+		return employeeName;
 	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
+
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
 	}
+
+
 	public String getDesignation() {
 		return designation;
 	}
+
+
 	public void setDesignation(String designation) {
 		this.designation = designation;
 	}
-	public String geteAddrOne() {
-		return eAddrOne;
+
+
+	public String getEmployeeAddressOne() {
+		return employeeAddressOne;
 	}
-	public void seteAddrOne(String eAddrOne) {
-		this.eAddrOne = eAddrOne;
+
+
+	public void setEmployeeAddressOne(String employeeAddressOne) {
+		this.employeeAddressOne = employeeAddressOne;
 	}
-	public String geteAddrTwo() {
-		return eAddrTwo;
+
+
+	public String getEmployeeAddressTwo() {
+		return employeeAddressTwo;
 	}
-	public void seteAddrTwo(String eAddrTwo) {
-		this.eAddrTwo = eAddrTwo;
+
+
+	public void setEmployeeAddressTwo(String employeeAddressTwo) {
+		this.employeeAddressTwo = employeeAddressTwo;
 	}
-	public String geteCity() {
-		return eCity;
+
+
+	public String getEmployeeCity() {
+		return employeeCity;
 	}
-	public void seteCity(String eCity) {
-		this.eCity = eCity;
+
+
+	public void setEmployeeCity(String employeeCity) {
+		this.employeeCity = employeeCity;
 	}
-	public String geteState() {
-		return eState;
+
+
+	public String getEmployeeState() {
+		return employeeState;
 	}
-	public void seteState(String eState) {
-		this.eState = eState;
+
+
+	public void setEmployeeState(String employeeState) {
+		this.employeeState = employeeState;
 	}
-	public Integer getSsnNumber() {
+
+
+	public Long getSsnNumber() {
 		return ssnNumber;
 	}
-	public void setSsnNumber(Integer ssnNumber) {
+
+
+	public void setSsnNumber(Long ssnNumber) {
 		this.ssnNumber = ssnNumber;
 	}
-	public Integer getHomePhone() {
+
+
+	public Long getHomePhone() {
 		return homePhone;
 	}
-	public void setHomePhone(Integer homePhone) {
+
+
+	public void setHomePhone(Long homePhone) {
 		this.homePhone = homePhone;
 	}
-	public Integer getOfficePhone() {
+
+
+	public Long getOfficePhone() {
 		return officePhone;
 	}
-	public void setOfficePhone(Integer officePhone) {
+
+
+	public void setOfficePhone(Long officePhone) {
 		this.officePhone = officePhone;
 	}
-	public Integer getMobile() {
+
+
+	public Long getMobile() {
 		return mobile;
 	}
-	public void setMobile(Integer mobile) {
+
+
+	public void setMobile(Long mobile) {
 		this.mobile = mobile;
 	}
-	public Integer getPincode() {
+
+
+	public Long getPincode() {
 		return pincode;
 	}
-	public void setPincode(Integer pincode) {
+
+
+	public void setPincode(Long pincode) {
 		this.pincode = pincode;
 	}
+
+
 	public Integer getYears() {
 		return years;
 	}
+
+
 	public void setYears(Integer years) {
 		this.years = years;
 	}
+
+
 	public Integer getMonths() {
 		return months;
 	}
+
+
 	public void setMonths(Integer months) {
 		this.months = months;
 	}
-	public Integer getePincode() {
-		return ePincode;
+
+
+	public Integer getEmployeePincode() {
+		return employeePincode;
 	}
-	public void setePincode(Integer ePincode) {
-		this.ePincode = ePincode;
+
+
+	public void setEmployeePincode(Integer employeePincode) {
+		this.employeePincode = employeePincode;
 	}
+
+
 	public Double getLoanAmt() {
 		return loanAmt;
 	}
+
+
 	public void setLoanAmt(Double loanAmt) {
 		this.loanAmt = loanAmt;
 	}
+
+
 	public Double getAnnualSal() {
 		return annualSal;
 	}
+
+
 	public void setAnnualSal(Double annualSal) {
 		this.annualSal = annualSal;
 	}
-	
 	
 	
 	
